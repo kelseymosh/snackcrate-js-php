208ac7128f2458700bf19ee2e91964ec74b8d155
Kelsey Oshiro <kelseymosh@gmail.com>
2021-05-30 16:03:50 -0600

    Tic Tac Toe first commit

---
+++ ticTacToe.js
@@ -0,0 +1,20 @@
+//Write a function to create an 'X' by 'X' tic tac toe board defined by the "boardSize" value and make it playable.
+    //Retrieve information from a GET to dictate moves
+    //Key and example Values In the GET
+        //XY='0,3'
+        //turn=x
+        
+let boardSize = 3
+const board = document.getElementById("board")
+
+const createRows = (boardSize) => {
+    for (r = 0; r < boardSize; r++){
+        let div = document.createElement("div")
+        board.append(div).className = "square"
+    }
+}
+
+const createColumns = (boardSize) => {
+
+}
+
