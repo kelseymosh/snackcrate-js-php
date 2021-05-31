ee30405ad07935ee1fe444d43513e453b9dfca40
Kelsey Oshiro <kelseymosh@gmail.com>
2021-05-31 17:42:04 -0600

    Get today's date

--- assessment.php
+++ assessment.php
@@ -16,9 +16,7 @@ echo $_SERVER['HTTP_CLIENT_IP'] ? : ($_SERVER['HTTP_X_FORWARDED_FOR'] ? : $_SERV
 
 <!-- //Write a function to get today's date in this format "2014-02-13 02:42:48" -->
 <?php
-$time = gmmktime();
-echo date("Y-m-d H:i:s", $time); 
+  date_default_timezone_set("America/Denver");
+  $currentDateTime = date('Y-m-d H:i:s');
+  echo $currentDateTime;
 ?>
-
-<!-- //Bonus
-//Create a class called "userController" write methods to handle the login process, logout process, account creation, password reset, and account information updates.  -->
